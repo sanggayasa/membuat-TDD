dd891add22f7dfe67e6913e7a2bf2278d00cc80b
sanggayasa <sanggayasa502@gmail.com>
2021-12-17 19:06:44 +0700

    selesai penggunaaan code cept dan testing

--- e2e/Lingking_Movies.spec.js
+++ e2e/Lingking_Movies.spec.js
@@ -1,24 +1,54 @@
+/* eslint-disable no-undef */
 Feature('Liking Movies');
+const assert = require('assert');
 
 Before(({ I }) => {
   I.amOnPage('/#/like');
 });
 
-Scenario('showing empty liked movies', ({ I }) => {
+Scenario('showing empty liked movies', async ({ I }) => {
   I.seeElement('#query');
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
 });
 
-Scenario('liking one movie', ({ I }) => {
+Scenario('liking one movie', async ({ I }) => {
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
 
   I.amOnPage('/');
+  // … kita akan mengisi uji coba berikutnya …
   I.seeElement('.movie__title a');
-  I.click(locate('.movie__title a').first());
+
+  const firstFilm = locate('.movie__title a').first();
+  const firstFilmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/like');
   I.seeElement('.movie-item');
+  const likedFilmTitle = await I.grabTextFrom('.movie__title');
+
+  assert.strictEqual(firstFilmTitle, likedFilmTitle);
+});
+
+Scenario('searching movies', async ({ I }) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.movie__title a');
+
+  const titles = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.movie__title a').at(i));
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+    titles.push(await I.grabTextFrom('.movie__title'));
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/like');
+  I.seeElement('#query');
 });
